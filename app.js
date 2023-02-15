46f75e43925eba52de3db55b658ab4a4c12b2691
Andrey Ilyichev <gidroskolbasovich@gmail.com>
2023-02-15 14:08:25 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const sdk = new window.Appwrite();
+sdk.setEndpoint("http://192.168.0.101:80/v1").setProject("63dd026b480ab089a636");
+
+const urlParams = new URLSearchParams(window.location.search);
+const userId = urlParams.get('userId');
+const secret = urlParams.get('secret');
+
+let promise = sdk.account.updateVerification(userId,secret);
+promise.then(function (res){
+    console.log(res);
+    document.getElementById("message").innerText = JSON.stringify(res);
+    window.location.replace('appwrite-callback-63dd026b480ab089a636://verified');
+
+},function (error){
+    console.log(error);
+    document.getElementById("message").innerText = JSON.stringify(error);
+})
