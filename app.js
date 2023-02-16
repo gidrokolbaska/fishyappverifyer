401fb118f3af4084468af432ab7b9e5fdafa2ff5
Andrey Ilyichev <gidroskolbasovich@gmail.com>
2023-02-16 11:56:28 +0300

    changed endpoint

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const sdk = new window.Appwrite();
-sdk.setEndpoint("http://192.168.0.101:80/v1").setProject("63dd026b480ab089a636");
+sdk.setEndpoint("http://appwrite.fishyapp.ru/v1").setProject("63dd026b480ab089a636");
 
 const urlParams = new URLSearchParams(window.location.search);
 const userId = urlParams.get('userId');
