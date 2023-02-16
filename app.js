fde3816d6acdd13a79909c4dab3d8ca39781fd96
Andrey Ilyichev <gidroskolbasovich@gmail.com>
2023-02-16 12:29:48 +0300

    test

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ let promise = sdk.account.updateVerification(userId,secret);
 promise.then(function (res){
     console.log(res);
     document.getElementById("message").innerText = JSON.stringify(res);
-    window.location.replace('appwrite-callback-63dd026b480ab089a636://verified');
+    window.location.replace('appwrite-callback-63dd026b480ab089a636');
 
 },function (error){
     console.log(error);
